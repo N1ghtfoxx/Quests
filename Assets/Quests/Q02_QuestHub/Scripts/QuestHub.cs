c4622ef49ffa6f2fa3f97b3c6563f41e5acd6481
N1ghtfoxx <2024.gme.naomizel@gmail.com>
2026-01-04 18:40:51 +0100

    UPD Script 'QuestHub' UPD Scene 'Q01_ClickGrow' - added QuestHubManager UPD Scene 'Q02_QuestHub' - first button has now ability to load scene Q01

--- Assets/Quests/Q02_QuestHub/Scripts/QuestHub.cs
+++ Assets/Quests/Q02_QuestHub/Scripts/QuestHub.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class QuestHub : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string hubSceneName = "Q02_QuestHub"; // Name of the hub scene
+
+    private InputSystem_Actions inputActions;
+
+    // initialize input actions
+    private void Awake()
     {
-        
+        inputActions = new InputSystem_Actions();
     }
 
-    // Update is called once per frame
-    void Update()
+    // enable input actions when the object is enabled
+    private void OnEnable()
     {
-        
+        inputActions.UI.Enable();
+        inputActions.UI.Cancel.performed += OnCancel;
+    }
+
+    // disable input actions when the object is disabled
+    private void OnDisable()
+    {
+        inputActions.UI.Cancel.performed -= OnCancel;
+        inputActions.UI.Disable();
+    }
+
+    // method called when the Cancel action is performed
+    private void OnCancel(InputAction.CallbackContext context)
+    {
+        // Load the hub scene when 'ESC' is pressed
+        LoadHubScene();
+    }
+
+    // method to load the hub scene
+    public void LoadHubScene()
+    {
+        SceneManager.LoadScene(hubSceneName);
+    }
+
+    // method to load Quest 1 scene (can be duplicated for other quests)
+    public void LoadQuest1Scene()
+    {
+               SceneManager.LoadScene("Q01_ClickGrow");
     }
 }
